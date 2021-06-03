0abb987b8454f08a7f5450787ed3375588136b90
zhang kai <kai_zhang@autobrain.cn>
2021-06-03 23:27:20 +0800

    初次提交

---
+++ DogCatDataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+class DogCatDataset(Dataset)
+    def __init__(self, root_path):
+        self.label_name = {"Cat": 0, "Dog": 1}
+        self.root_path = root_path
+        #self.img_name =    #路径下图片名称集合
+        #self.label =       #与名称对应的label集合
+
+
+    def __getitem__():
+
+    def __len__():
+
+    
